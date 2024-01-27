9d74067303cb26b972049625dd73ca93aa81d932
Yuliya Kobrina <JulieBrase@gmail.com>
2024-01-27 21:40:40 +0300

    task 1 complete

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b**2) - (4*a*c);
+
+  if (d < 0 ) {
+    arr = [];
+  } else if (d === 0) {
+    let x = -b/(2*a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2)
+  }
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage (percent, contribution, amount, countMonths) {
+totalDebth = amount - contribution;
+monthInterest = (percent / 100) / 12;
+payment = totalDebth * (monthInterest + (monthInterest / (((1 + monthInterest) ** countMonths) - 1)));
+totalAmountToPay = (payment * countMonths).toFixed(2);
+return Number(totalAmountToPay);  
 }
+
+
