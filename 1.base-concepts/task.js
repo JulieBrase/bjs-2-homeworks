20f0478cf5af5d4f6e9bce5f6bf308f9b37f2142
Yuliya Kobrina <JulieBrase@gmail.com>
2024-02-10 16:30:57 +0300

    fixed the notes

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,27 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = (b**2) - (4*a*c);
+  let d = b ** 2 - 4 * a * c;
 
-  if (d < 0 ) {
-    arr = [];
-  } else if (d === 0) {
-    let x = -b/(2*a);
+  if (d === 0) {
+    let x = -b / (2 * a);
     arr.push(x);
-  } else {
-    let x1 = (-b + Math.sqrt(d))/(2*a);
-    let x2 = (-b - Math.sqrt(d))/(2*a);
-    arr.push(x1, x2)
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
   }
   return arr;
 }
 
-function calculateTotalMortgage (percent, contribution, amount, countMonths) {
-totalDebth = amount - contribution;
-monthInterest = (percent / 100) / 12;
-payment = totalDebth * (monthInterest + (monthInterest / (((1 + monthInterest) ** countMonths) - 1)));
-totalAmountToPay = (payment * countMonths).toFixed(2);
-return Number(totalAmountToPay);  
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let totalDebth = amount - contribution;
+  let monthInterest = percent / 100 / 12;
+  let payment =
+    totalDebth *
+    (monthInterest + monthInterest / ((1 + monthInterest) ** countMonths - 1));
+  //let totalAmountToPay = (payment * countMonths).toFixed(2);
+  return Number((payment * countMonths).toFixed(2));
 }
-
-
