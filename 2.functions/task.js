6c9a4d337746210d972d1363ec24cbca8a365cc0
Yuliya Kobrina <JulieBrase@gmail.com>
2024-03-23 20:50:28 +0300

    Test 1 completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,20 @@
 function getArrayParams(...arr) {
-
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  let avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+function summElementsWorker(...arr) {}
 
-}
+function differenceMaxMinWorker(...arr) {}
 
-function differenceMaxMinWorker(...arr) {
+function differenceEvenOddWorker(...arr) {}
 
-}
+function averageEvenElementsWorker(...arr) {}
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+function makeWork(arrOfArr, func) {}
